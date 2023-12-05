3b68034ae5c753adf9dc8beb43ca4d5f0a5e9607
Ramel Cabug-os <ramelcabug-os@Ramels-MacBook-Pro-2.local>
2023-12-05 14:51:32 +0800

    gates

--- db/migrate/20231205052801_create_gates.rb
+++ db/migrate/20231205052801_create_gates.rb
@@ -2,7 +2,7 @@ class CreateGates < ActiveRecord::Migration[7.1]
   def change
     create_table :gates do |t|
       t.string :serial_number
-
+      t.string :name
       t.timestamps
     end
   end
